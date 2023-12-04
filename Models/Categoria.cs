87c7a279003e8f6c6e76e8769621ef571ccf8c0a
tassianabenamor <tassiana.benamor@gmail.com>
2023-12-03 21:09:39 -0300

    Models

---
+++ Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace OiBobaLoja.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Descricao { get; set; }
+
+        public ICollection<Print>? Prints { get; set; }
+    }
+}
---
+++ Models/Marca.cs
@@ -0,0 +1,10 @@
+﻿namespace OiBobaLoja.Models
+{
+    public class Marca
+    {
+        public int MarcaId { get; set; }
+        public string Descricao { get; set; }
+
+        public ICollection<Print> Prints { get; set; }
+    }
+}
